800ab6e4e582648a4d042e2e22cfc424523fdf3f
Apeksha-P <pavithriapeksha1new@gmail.com>
2023-11-26 10:57:50 +0530

    Number of digits in user enter number:

--- src/Question_2.java
+++ src/Question_2.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Question_2 {
     public static void main(String[] args) {
-
+        Scanner number = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int x = number.nextInt();
+        int digit = countDigit(x);
+        System.out.println("Number of digits in "+x+": "+digit);
+    }
+    public static int countDigit(int x){
+        x=Math.abs(x);
+        if (x==0){
+            return 1;
+        }
+        int count=0;
+        while(x>0){
+            x=x/10;
+            count++;
+        }
+        return count;
     }
 }
