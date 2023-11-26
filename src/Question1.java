4ebe32af1347131a46c09fa0c73d282253cb67c1
Apeksha-P <pavithriapeksha1new@gmail.com>
2023-11-26 10:35:43 +0530

    new class

---
+++ src/Question1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Question1 {
+    public static void main(String[] args) {
+        int x=10;
+        for (int i=0;i<4;i++){
+            for (int j=0;j<10;j++){
+                System.out.print(x+" ");
+                x++;
+            }
+            System.out.println();
+        }
+    }
+}
