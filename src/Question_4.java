bb7405a6bfe1f5bc48f6afa05947a8c52715e721
Apeksha-P <pavithriapeksha1new@gmail.com>
2023-11-28 15:56:55 +0530

    Find second larger number in Array

---
+++ src/Question_4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Question_4 {
+    public static void main(String[] args) {
+        Scanner number = new Scanner(System.in);
+        int[] num =new int[5];
+        for (int i=0;i<5;i++){
+            System.out.println("Eneter number "+(i+1)+" :");
+            int x= number.nextInt();
+            num[i]=x;
+        }
+        int secondLargest = findSecondLarger(num);
+        System.out.println("Second large number is : "+secondLargest);
+
+    }
+    public static int findSecondLarger(int[] num){
+        Arrays.sort(num);
+        return num[1];
+    }
+}
